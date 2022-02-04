3555febbee725a381ec4dd0829d3ba0ade61f20e
Diana Martinez <diananerdoficial@gmail.com>
2022-02-04 15:12:43 -0600

    base

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <UserRepositories :user="'Diana'"></UserRepositories>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import UserRepositories from "./components/UserRepositories.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    UserRepositories,
   },
 };
 </script>
---
+++ src/components/UserRepositories.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>{{ user }} Repositories</div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      repositories: [], // 1
+      filters: {}, // 3
+      searchQuery: "", // 2
+    };
+  },
+  computed: {
+    filteredRepositories() {
+      // 3
+      return null;
+    },
+    repositoriesMatchingSearchQuery() {
+      // 2
+      return null;
+    },
+  },
+};
+</script>
