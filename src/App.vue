b4918f22c6b8e7770161234946e8a782d53cff32
Diana Martinez <diananerdoficial@gmail.com>
2022-02-03 18:02:55 -0600

    Teleports

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Modal />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Modal,
   },
 };
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="toggle">Modal</button>
+    <teleport to="body">
+      <div v-show="show" class="modal">
+        <h1>Titulo</h1>
+        <p>Lorem ipsum dolor</p>
+        <button @click="toggle">Cerrar</button>
+      </div>
+    </teleport>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
