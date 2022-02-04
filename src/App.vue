476de20031fa441d9f65386e6c337b4d138dc24b
Diana Martinez <diananerdoficial@gmail.com>
2022-02-03 18:18:56 -0600

    base

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
