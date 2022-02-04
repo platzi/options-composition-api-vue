c04b253abf65be66aaa32d10e1a2187fef088c83
Diana Martinez <diananerdoficial@gmail.com>
2022-02-04 16:08:07 -0600

    Watchets

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,13 +3,20 @@
 </template>
 
 <script>
-import { reactive } from "vue";
+import { reactive, watch } from "vue";
 
 export default {
   setup() {
     const obj = reactive({ counter: 0 });
 
     setInterval(() => obj.counter++, 500);
+
+    watch(
+      () => obj.counter,
+      (valor, anterior) => {
+        console.log(valor, anterior);
+      }
+    );
 
     return {
       obj,
