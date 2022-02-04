016c92daa0f3d1ad9df0d1ae7e9f55840ac12044
Diana Martinez <diananerdoficial@gmail.com>
2022-02-04 15:00:11 -0600

    Transiciones

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button @click="show = !show">Menu</button>
+  <transition name="fade">
+    <Menu v-show="show" />
+  </transition>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Menu from "./components/Menu.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { Menu },
+  data() {
+    return {
+      show: false,
+    };
   },
 };
 </script>
@@ -22,5 +26,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-leave-active,
+.fade-enter-active {
+  transition: opacity 0.5s ease;
 }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul>
+    <li>Opcion 1</li>
+    <li>Opcion 2</li>
+    <li>Opcion 3</li>
+  </ul>
+</template>
