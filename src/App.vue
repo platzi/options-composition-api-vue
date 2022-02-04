1c93b24cdb819a06de3c5b85e17f18a3373e6def
Diana Martinez <diananerdoficial@gmail.com>
2022-02-03 18:44:13 -0600

    Ciclo de vida

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,15 @@ export default {
       text: "Hola Vue",
     };
   },
+  beforeCreate() {
+    console.log("beforeCreate", this.$data, this.$el);
+  },
+  created() {
+    console.log("created", this.$data, this.$el);
+  },
+  mounted() {
+    console.log("mounted", this.$data, this.$el);
+  },
 };
 </script>
 
