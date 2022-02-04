ac8ba21d8dae41b03f7d3fdfbd08ebe40c11fbe8
Diana Martinez <diananerdoficial@gmail.com>
2022-02-04 15:31:19 -0600

    base

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  <div></div>
+  <Home></Home>
 </template>
 
 <script>
-export default {};
+import Home from "@/components/Home.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
