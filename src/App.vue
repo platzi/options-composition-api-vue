2e12f00d357eeb7273ca142df16e4018dee1c5d7
Diana Martinez <diananerdoficial@gmail.com>
2022-02-04 18:41:00 -0600

    Ciclo de vida Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <UserRepositories :user="'Diana'"></UserRepositories>
+  <Home></Home>
 </template>
 
 <script>
-import UserRepositories from "./components/UserRepositories.vue";
+import Home from "./components/Home.vue";
 
 export default {
   name: "App",
   components: {
-    UserRepositories,
+    Home,
   },
 };
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hola</div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+export default {
+  setup() {
+    onMounted(() => {
+      console.log("mounted!");
+    });
+  },
+};
+</script>
